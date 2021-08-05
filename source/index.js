55be88d50d2ec0f976e3c8b4925bdf9a716a6912
Perry Mitchell <perry@perrymitchell.net>
2021-08-05 11:37:43 +0300

    Add support for env variables

--- source/index.js
+++ source/index.js
@@ -24,36 +24,46 @@ const getCredentialsClass = () => require("aws-sdk").Credentials;
  * @param {String=} pathOverride Optional override for the credentials path
  *  (defaults first to the environment variable `AWS_CREDENTIALS_PATH`, and
  *  then to `~/.aws/credentials` lastly)
+ * @param {Boolean=} useEnvironmentVariables Optionally allow environment
+ *  variables to provide the Credentials instance. This disregards the profile
+ *  and assumes that the token in the environment variables is desired. Does
+ *  not read the credentials file if the envirnment variables
+ *  "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY" are present. Defaults to
+ *  false.
  * @returns {Promise.<AWS.Credentials>} A promise that resolves with the AWS
  *  Credentials instance
  * @memberof module:GetAWSCredentials
  */
-function getAWSCredentials(profileOverride, pathOverride) {
+async function getAWSCredentials(profileOverride, pathOverride, useEnvironmentVariables = false) {
     const Credentials = getCredentialsClass();
+    if (useEnvironmentVariables) {
+        const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
+        if (AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY) {
+            return new Credentials(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY);
+        }
+    }
     const awsCredentialsPath =
         pathOverride ||
         process.env.AWS_CREDENTIALS_PATH ||
         path.resolve(os.homedir(), "./.aws/credentials");
     const awsCredentialsProfile =
         profileOverride || process.env.AWS_PROFILE || process.env.AWS_DEFAULT_PROFILE || "default";
-    return readFile(awsCredentialsPath, "utf8")
-        .then(rawData => ini.parse(rawData))
-        .then(credentialsData => {
-            const {
-                aws_access_key_id: accessKey,
-                aws_secret_access_key: secretKey
-            } = credentialsData[awsCredentialsProfile];
-            if (typeof accessKey !== "string") {
-                throw new VError(`No access key ID for profile: ${awsCredentialsProfile}`);
-            }
-            if (typeof secretKey !== "string") {
-                throw new VError(`No secret access key for profile: ${awsCredentialsProfile}`);
-            }
-            return new Credentials(accessKey, secretKey);
-        })
-        .catch(error => {
-            throw new VError(error, "Failed getting credentials");
-        });
+    const rawData = await readFile(awsCredentialsPath, "utf8");
+    const credentialsData = ini.parse(rawData);
+    const { aws_access_key_id: accessKey, aws_secret_access_key: secretKey } = credentialsData[
+        awsCredentialsProfile
+    ];
+    if (typeof accessKey !== "string") {
+        throw new VError(
+            `Failed getting credentials: No access key ID for profile: ${awsCredentialsProfile}`
+        );
+    }
+    if (typeof secretKey !== "string") {
+        throw new VError(
+            `Failed getting credentials: No secret access key for profile: ${awsCredentialsProfile}`
+        );
+    }
+    return new Credentials(accessKey, secretKey);
 }
 
 /**
