1fc235d8e927a48f7d194fabfdd3ea17c479bf44
Perry Mitchell <perry@perrymitchell.net>
2021-08-05 11:37:53 +0300

    Add tests for env vars

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -6,10 +6,25 @@ describe("index", function() {
     const SAMPLE = path.resolve(__dirname, "../sample-credentials");
 
     describe("getAWSCredentials", function() {
-        it("returns expected credentials", function() {
-            return getAWSCredentials("mycompany-production", SAMPLE).then(creds => {
-                expect(creds).to.be.an.instanceOf(Credentials);
-            });
+        afterEach(function() {
+            process.env.AWS_ACCESS_KEY_ID = undefined;
+            process.env.AWS_SECRET_ACCESS_KEY = undefined;
+        });
+
+        it("returns expected credentials", async function() {
+            const creds = await getAWSCredentials("mycompany-production", SAMPLE);
+            expect(creds).to.be.an.instanceOf(Credentials);
+            expect(creds.accessKeyId).to.equal("AAABBBCCCDDD");
+            expect(creds.secretAccessKey).to.equal("YXNkYXNmc2Rmc2RmZ2RnZGZnZGdkZmdk");
+        });
+
+        it("returns expected credentials when using environment variables", async function() {
+            process.env.AWS_ACCESS_KEY_ID = "test1";
+            process.env.AWS_SECRET_ACCESS_KEY = "test2";
+            const creds = await getAWSCredentials("mycompany-production", SAMPLE, true);
+            expect(creds).to.be.an.instanceOf(Credentials);
+            expect(creds.accessKeyId).to.equal("test1");
+            expect(creds.secretAccessKey).to.equal("test2");
         });
     });
 
